c5b83590caa88fd1598af9f40d5fb8291a58c410
smekers <castor.rachel05@gmail.com>
2023-12-19 13:59:12 +0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FDCAssessment
+//
+//  Created by d on 12/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
